96182da4e0e4a53bb3c4b0c7ebd2c1545948b4b1
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-02 14:59:18 +0700

    Offline Info dan manifest

--- src/App.js
+++ src/App.js
@@ -6,29 +6,47 @@ import Clients from "./components/Clients";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Offline from "./components/Offline";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [offlineStatus, setOfflineStatus] = useState(!navigator.onLine);
+
+  const handleOfflineStatus = () => setOfflineStatus(!navigator.onLine);
 
   useEffect(() => {
     (async function () {
-      const response = await fetch(
-        "https://bwacharity.fly.dev/items",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            accept: "application/json",
-            // "x-api-key": process.env.REACT_APP_APIKEY,
-          },
-        }
-      );
+      const response = await fetch("https://bwacharity.fly.dev/items", {
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+          // "x-api-key": process.env.REACT_APP_APIKEY,
+        },
+      });
       const { nodes } = await response.json();
       setItems(nodes);
+
+      if (!document.querySelector('script[src="/carousel.js"]')) {
+        const script = document.createElement("script");
+        script.src = "/carousel.js";
+        script.async = false;
+        document.body.appendChild(script);
+      }
     })();
-  }, []);
+
+    handleOfflineStatus();
+    window.addEventListener('online', handleOfflineStatus);
+    window.addEventListener('offline', handleOfflineStatus);
+
+    return () => {
+      window.removeEventListener('online', handleOfflineStatus);
+      window.removeEventListener('offline', handleOfflineStatus);
+    }
+  }, [offlineStatus]);
 
   return (
     <>
+    {offlineStatus && <Offline />}
       <Header />
       <Hero />
       <Browse />
